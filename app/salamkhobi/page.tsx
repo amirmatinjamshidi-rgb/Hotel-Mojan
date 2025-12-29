fe05b2607059fc24a4e94b4dffa5e586cc8f3572
AmirK <amirkhoramdelaval@gmail.com>
2025-12-29 20:59:17 +0330

    user profile done

--- app/salamkhobi/page.tsx
+++ app/salamkhobi/page.tsx
@@ -1,10 +1,11 @@
 
+import UserProfile from "@/Features/components/UserPanel/UserAccount/UserProfile";
 import React from "react";
 
 function page() {
   return (
     <div className="flex flex-col justify-center items-center pt-10">
-      
+      <UserProfile />
     </div>
   );
 }
